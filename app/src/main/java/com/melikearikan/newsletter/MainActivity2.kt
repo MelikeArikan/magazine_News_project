f42090c30d328fd26cf32415f3b3db7dd5852ea4
Melike ARIKAN <melikearikandev@gmail.com>
2022-08-31 14:28:08 +0300

    kaydedildi..

--- app/src/main/java/com/melikearikan/newsletter/MainActivity2.kt
+++ app/src/main/java/com/melikearikan/newsletter/MainActivity2.kt
@@ -44,8 +44,8 @@ class MainActivity2 : AppCompatActivity() {
         haberDetaylari.add("Arnavutluk İçişleri Bakanı Bledar Çuçi, bugün sabah saatlerinde İçişleri Bakanı Süleyman Soylu’ya kırmızı bültenle aranan Thodex’in kurucusu firari Faruk Fatih Özer'in Arnavutluk Vlora’da yakalandığını ve kimliğinin biyometrik sonuçlar ile de teyit edildiğini iletti.Emniyet Genel Genel Müdürlüğü İnterpol Daire Başkanlığınca Fatih Özer’in Türkiye’ye iadesi işlemlerine başlandı.Öte yandan İçişleri Bakanlığı'ndan, Thodex'in kurucusu Fatih Özer'in iadesiyle ilgili bir açıklama yapıldı.İçişleri Bakanlığından yapılan açıklamada şu ifadelere yer verildi:“Arnavutluk Cumhuriyeti İçişleri Bakanı Bledar Çuçi bugün sabah saatlerinde İçişleri Bakanı Süleyman Soylu’ya Kırmızı Bültenle aranan Thodex’in kurucusu firari Faruk Fatih Özer'in Arnavutluk’ta Vlora’da yakalandığını ve  kimliğinin biyometrik sonuçlar ile teyit edildiğini iletti.Emniyet Genel Genel Müdürlüğü İnterpol Daire Başkanlığınca, Fatih Özer’in Türkiye’ye iadesi işlemlerine başlandı.”")
 
         var detay_resimler = arrayListOf<Int>()
-        detay_resimler.add(R.drawable.detaylar1)
-        detay_resimler.add(R.drawable.img_8)
+        detay_resimler.add(R.drawable.thodex)
+        detay_resimler.add(R.drawable.detayli_kaza2)
         detay_resimler.add(R.drawable.img_9)
         detay_resimler.add(R.drawable.img_12)
         detay_resimler.add(R.drawable.img_10)
